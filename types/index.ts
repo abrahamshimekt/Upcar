34b60293d4be1dd78d4d67564e1b4cf96c55df93
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2024-12-21 23:48:19 +0300

    add car catalogue (#1)
    LGTM

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,24 @@
 import { MouseEventHandler } from "react";
 
-export interface CustomButtonProps{
-    title:string,
-    btnType?:"button"| "submit",
-    containerStyle:string,
-    handleClick?: MouseEventHandler<HTMLButtonElement>
+export interface CustomButtonProps {
+  title: string;
+  textStyles: string;
+  rightIcon: string;
+  btnType?: "button" | "submit";
+  containerStyles: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }

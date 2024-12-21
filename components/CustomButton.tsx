4e46d6e99c50cdbb8c1ba1134bdb092d2fd45119
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-21 23:46:33 +0300

    add car catalogue

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,16 +1,34 @@
 "use client";
 import { CustomButtonProps } from "@/types";
+import Image from "next/image";
 import React from "react";
 
-const CustomButton = ({title,btnType,containerStyle,handleClick}:CustomButtonProps) => {
+const CustomButton = ({
+  title,
+  textStyles,
+  rightIcon,
+  btnType,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
       type={btnType}
-      className={`custom-btn ${containerStyle}`}
+      className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-        <span className="flex-1">{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="arrow right "
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,19 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-export { Hero,CustomButton,Navbar,Footer };
+import SearchBar from "./Searchbar";
+import SearchManufacturer from "./SearchManufacturer";
+import CustomFilter from "./CustomFilter";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  SearchManufacturer,
+  CustomFilter,
+  CarCard,
+  ShowMore,
+};

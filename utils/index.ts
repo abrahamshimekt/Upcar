8508f915a08b10705470b80756c68ffee4493b52
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2024-12-22 14:28:12 +0300

    add filter by fuel and year (#3)
    Look Good To Me

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
 import { CarProps, FilterProps } from "@/types";
 export async function fetchCars(filters: FilterProps) {
-  const {manufacturer,year,model,limit,fuel} = filters;
+  const { manufacturer, year, model, limit, fuel } = filters;
   const headers: HeadersInit = {
     "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
@@ -41,4 +41,11 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append("modelYear", `${year}`);
   url.searchParams.append("angle", `${angle}`);
   return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.set(type, value);
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  return newPathname;
 };

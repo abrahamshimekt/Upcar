bd5579914e5d67bbe423525f2d964990f4a304ba
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-22 17:50:20 +0300

    group

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,15 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import CustomButton from "./CustomButton";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/sign-in");
+  };
   return (
     <header className="w-full absolute z-10">
       <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
@@ -19,7 +25,8 @@ const Navbar = () => {
         <CustomButton
           title="Sign In"
           btnType="button"
-          containerStyle="text-primary-blue rounded-full bg-white min-w-[130px]"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+          handleClick={handleClick}
         />
       </nav>
     </header>

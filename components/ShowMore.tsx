3d47da81b0a4384263183c83cad1fb3a03325fe2
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2024-12-22 14:59:08 +0300

    Show more (#4)
    * add filter by fuel and year
    
    * add more button and fuctionality

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+"use client";
+import { ShowMoreProps } from "@/types";
+import { useRouter } from "next/navigation";
+import React from "react";
+import CustomButton from "./CustomButton";
+import { updateSearchParams } from "@/utils";
 
-const ShowMore = () => {
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newpathname = updateSearchParams("limit", `${newLimit}`);
+    router.push(newpathname, { scroll: false });
+  };
   return (
-    <div>ShowMore</div>
-  )
-}
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
 
-export default ShowMore
+export default ShowMore;
